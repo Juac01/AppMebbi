4f2131f85f794832fa19365314319b8c29533d37
juac01 <julcamoro1998@hotmail.com>
2024-10-16 13:35:34 -0300

    Ok

---
+++ AppMebbi/CoreDataTeste.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataTeste.swift
+//  AppMebbi
+//
+//  Created by JOSE JOAQUIN JULCAMORO BUSTAMANTE on 16/10/24.
+//
+
+import SwiftUI
+
+struct CoreDataTeste: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreDataTeste()
+}
---
+++ AppMebbi/Notificacoes.swift
@@ -0,0 +1,18 @@
+//
+//  Notificacoes.swift
+//  AppMebbi
+//
+//  Created by JOSE JOAQUIN JULCAMORO BUSTAMANTE on 16/10/24.
+//
+
+import SwiftUI
+
+struct Notificacoes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Notificacoes()
+}
